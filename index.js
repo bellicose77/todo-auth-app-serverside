2f56e275bf8e53e829701d240bb4645106b2d59f
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-06-26 00:53:33 +0600

    mongdb connection

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+TODODB
+Ig9ob18XsqRwLcak
+const uri = "mongodb+srv://<username>:<password>@cluster0.vtwog.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(){
     try{
