dda79d046ccb18dbfc41230a0ee684194e781000
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-06-26 00:38:19 +0600

    mongdb connection

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const port = 5000
 
-
+async function run(){
+    
+}
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
