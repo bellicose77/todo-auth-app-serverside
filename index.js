24d39caa2b909c2d2f19de60c473fe10b5a176b0
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-06-26 00:40:58 +0600

    mongdb connection

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const port = 5000;
 
 async function run(){
-    
+    try{
+        await client.connect();
+    }
+    finally{
+
+    }
+
 }
 
 app.listen(port, () => {
