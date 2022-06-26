f7e23a5dfbd1e5d51d7503b7c8c04ae99b467059
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-06-26 22:33:07 +0600

    post api

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const {MongoClient,ServerApiVersion} = require('mongodb');
 require('dotenv').config();
 const app = express();
+app.use(cors());
 const port = 5000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vtwog.mongodb.net/?retryWrites=true&w=majority`;
 //console.log(uri);
