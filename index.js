2a1cdcff4d48af6a4e290571c42b5d796a0c594d
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-08-05 11:26:11 +0600

    update done

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-const port = 5000;
+const port = process.env.PORT || 5000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vtwog.mongodb.net/?retryWrites=true&w=majority`;
 //console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
